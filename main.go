1df4bb9cc65fb15a177b2a54640e73237be354ec
Akhil Datla <66145155+akhil-datla@users.noreply.github.com>
2022-06-22 16:53:33 -0700

    Add banner

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"main/components/courses"
 	"main/server"
 	"github.com/jasonlvhit/gocron"
+	"github.com/pterm/pterm"
 )
 
 func main() {
@@ -12,10 +13,16 @@ func main() {
 	flag.Parse()
 	courses.DownloadCourses()
 	go Schedule()
+	banner()
 	server.Start(*portPtr)
 }
 
 func Schedule() {
 	gocron.Every(1).Hour().Do(courses.DownloadCourses)
 	<-gocron.Start()
+}
+
+func banner() {
+	pterm.DefaultCenter.Print(pterm.DefaultHeader.WithFullWidth().WithBackgroundStyle(pterm.NewStyle(pterm.BgLightBlue)).WithMargin(10).Sprint("Quest For Excellence Learning Platform"))
+	pterm.Info.Println("(c)2022 by Akhil Datla")
 }
